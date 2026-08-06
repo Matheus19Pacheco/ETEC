d814259663a4baebd8e85b0a43d012a78d96624f
Matheus19Pacheco <matheuspacheco190311@gmail.com>
2026-08-06 18:17:16 -0300

    Delete Ex.2 L3.py

--- Ex.2 L3.py
+++
@@ -1,8 +0,0 @@
-usuario = input("Digite o nome de usuário: ")
-senha = input("Digite a senha: ")
-
-while senha == usuario:
-    print("Erro! A senha não pode ser igual ao usuário.")
-    senha = input("Digite outra senha: ")
-
-print("Cadastro realizado com sucesso!")
